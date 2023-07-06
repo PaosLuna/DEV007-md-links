c09840074de1f6c41203ad8fdec9d4ef41a29f28
PaosLuna <lunapp20@gmail.com>
2023-07-06 16:11:57 -0600

    función recursiva lista

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import fs, { Stats, stat } from 'fs';
-import path from 'path';
-import chalk from 'chalk';
-import { error } from 'console';
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
 
 // VERIFICA SI LA RUTA EXISTE
-export function routeExists(route){ 
-  if(fs.existsSync(route)){
-    console.log(chalk.bold.bgGreen('El archivo fue encontrado'));
+export function routeExists(route) {
+  if (fs.existsSync(route)) {
+    console.log(chalk.bold.bgGreen("El archivo fue encontrado"));
     return true;
   } else {
-    console.warn(chalk.bold.red("El archivo no fue encontrado"))
+    console.warn(chalk.bold.red("El archivo no fue encontrado"));
     return false;
   }
 }
 
 // VERIFICA SI LA RUTA ES ABSOLUTA
-export function routeAbsolute(route){
+export function routeAbsolute(route) {
   if (path.isAbsolute(route)) {
     return route;
   } else {
@@ -24,30 +23,35 @@ export function routeAbsolute(route){
 }
 
 // VERIFICAR SI ES UN DIRECTORIO O ARCHIVO
-export function fileOrDir(file) {
-fs.stat(file, (error, Stats) => {
-  if (Stats.isFile()) {
-    console.log('Es un archivo')
-  } else if (Stats.isDirectory()) {
-    console.log('Es un directorio')
-    fs.promises.readdir(file, 'utf-8')
-    .then(files => {
-      console.log(typeof files)
-      files.forEach((file) => {
-        console.log(`Los archivos del directorio son: ${file}`);
-      })
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }  
-});
+export function fileOrDir(route) {
+  let arrayFiles = []
+  const stats = fs.statSync(route);
+  if (stats.isFile()) {
+    arrayFiles.push(route);
+  } else if (stats.isDirectory()) {
+    const files = fs.readdirSync(route, "utf-8");
+    files.forEach((file) => {
+      const newRoute = path.join(route, file);
+      const statsNew = fs.statSync(newRoute);
+      if (statsNew.isFile()) {
+        arrayFiles.push(newRoute);
+      } else if (statsNew.isDirectory()) {
+        arrayFiles = arrayFiles.concat(fileOrDir(newRoute));
+      }
+    });
+  }
+  return arrayFiles
+}
+
+// FILTRAR SOLO LOS DE EXTENSIÓN MD
+export function getMdExtension(arrayFiles) {
+  return arrayFiles.filter(file => path.extname(file) === '.md');
 }
 
 // LEE UN ARCHIVO
 export function readFile(file) {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf-8', (error, data) => {
+    fs.readFile(file, "utf-8", (error, data) => {
       if (error) {
         reject(error);
       } else {
@@ -57,8 +61,3 @@ export function readFile(file) {
   });
 }
 
-// CONOCER LA EXTENSIÓN DE UN ARCHIVO
-export function fileExtension(file){
-  const extension = path.extname(file)
-  console.log(extension);
-}
