908c835b61727a0da6dbaf902aea8f45052046a4
PaosLuna <lunapp20@gmail.com>
2023-07-24 12:11:57 -0600

    Fix error

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ export function validateTrue(links) {
     if (link.match(/\[.+?\]\(.+?\)/g)) {
       let linkFalse = link.match(/\[.+?\]\(.+?\)/g);
       const linkObject = {
-        href: linkFalse[0].match(/https*?:([^"')\s]+)/)[0],
+        href: linkFalse[0].match(/https*?:([^"')\s]+)/),
         text: linkFalse[0].match(/\[(.*?)\]/)[1],
         file: ruta,
       }
